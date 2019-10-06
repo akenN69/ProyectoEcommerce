c00d3c994c887166aa2117d53fa2fca2ac530911
Seba_virtual <rata_6952@hotmail.com>
2019-10-06 19:50:47 -0300

    reference N to N is added between user and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_205744) do
+ActiveRecord::Schema.define(version: 2019_10_06_211032) do
+
+  create_table "orders", force: :cascade do |t|
+    t.boolean "payed"
+    t.integer "quantity_product"
+    t.float "total_price"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
